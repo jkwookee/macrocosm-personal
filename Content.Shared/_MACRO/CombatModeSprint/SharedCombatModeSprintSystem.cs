c383e3f0d4ef7fddcf839235f8bb083f40b6f0a5
Terkala <appleorange64@gmail.com>
2026-05-09 13:53:40 -0500

    Changing the SharedCombatModeSprintSystem
    Changing the ordering of how it handles impact damage and the sprint mode

--- Content.Shared/_MACRO/CombatModeSprint/SharedCombatModeSprintSystem.cs
+++ Content.Shared/_MACRO/CombatModeSprint/SharedCombatModeSprintSystem.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
 using Content.Shared.CombatMode;
 using Content.Shared.Damage.Systems;
 using Content.Shared.IdentityManagement;
 using Content.Shared.Movement.Systems;
 using Content.Shared.Popups;
+using Robust.Shared.GameObjects;
 using Robust.Shared.Player;
 
 namespace Content.Shared._MACRO.CombatModeSprint;
@@ -14,12 +16,25 @@ public abstract class SharedCombatModeSprintSystem : EntitySystem
     [Dependency] private readonly DamageOnHighSpeedImpactSystem _impact = default!;
     [Dependency] private readonly SharedPopupSystem _popup = default!;
 
+    /// <summary>
+    /// Last combat-mode state we applied impact collide settings for (avoids redundant Dirty).
+    /// </summary>
+    private readonly Dictionary<EntityUid, bool> _lastImpactCollideCombat = new();
+
     public override void Initialize()
     {
         base.Initialize();
 
-        SubscribeLocalEvent<CombatModeSprintComponent, ToggleCombatActionEvent>(OnToggleCombat);
+        SubscribeLocalEvent<CombatModeSprintComponent, ToggleCombatActionEvent>(
+            OnToggleCombat,
+            after: [typeof(SharedCombatModeSystem)]);
         SubscribeLocalEvent<CombatModeSprintComponent, RefreshMovementSpeedModifiersEvent>(OnRefreshMovespeed);
+        SubscribeLocalEvent<CombatModeSprintComponent, ComponentShutdown>(OnSprintShutdown);
+    }
+
+    private void OnSprintShutdown(Entity<CombatModeSprintComponent> ent, ref ComponentShutdown args)
+    {
+        _lastImpactCollideCombat.Remove(ent.Owner);
     }
 
     private void OnToggleCombat(Entity<CombatModeSprintComponent> ent, ref ToggleCombatActionEvent args)
@@ -33,18 +48,24 @@ public abstract class SharedCombatModeSprintSystem : EntitySystem
 
     private void OnRefreshMovespeed(Entity<CombatModeSprintComponent> ent, ref RefreshMovementSpeedModifiersEvent args)
     {
-        if (_combatMode.IsInCombatMode(ent))
-        {
+        var inCombat = _combatMode.IsInCombatMode(ent);
+
+        if (inCombat)
             args.ModifySpeed(ent.Comp.SprintCoefficient);
-            if (ent.Comp.DoImpactDamage)
-                _impact.ChangeCollide(ent, ent.Comp.MinimumSpeed, ent.Comp.StunSeconds, ent.Comp.DamageCooldown, ent.Comp.SpeedDamage);
-        }
         else
-        {
             args.ModifySpeed(1f);
-            if (ent.Comp.DoImpactDamage)
-                _impact.ChangeCollide(ent, ent.Comp.DefaultMinimumSpeed, ent.Comp.DefaultStunSeconds, ent.Comp.DefaultDamageCooldown, ent.Comp.DefaultSpeedDamage);
-        }
+
+        if (!ent.Comp.DoImpactDamage)
+            return;
+
+        if (_lastImpactCollideCombat.TryGetValue(ent.Owner, out var last) && last == inCombat)
+            return;
+
+        if (inCombat)
+            _impact.ChangeCollide(ent, ent.Comp.MinimumSpeed, ent.Comp.StunSeconds, ent.Comp.DamageCooldown, ent.Comp.SpeedDamage);
+        else
+            _impact.ChangeCollide(ent, ent.Comp.DefaultMinimumSpeed, ent.Comp.DefaultStunSeconds, ent.Comp.DefaultDamageCooldown, ent.Comp.DefaultSpeedDamage);
+
+        _lastImpactCollideCombat[ent.Owner] = inCombat;
     }
 }
-
