6723d68b8cd4b218cdf09d4592aeb13b286c8289
jkwookee <157201244+jkwookee@users.noreply.github.com>
2026-06-08 12:49:01 -0400

    fix exception

--- Content.Server/_MACRO/CombatMode/CombatModeVisualsSystem.cs
+++ Content.Server/_MACRO/CombatMode/CombatModeVisualsSystem.cs
@@ -1,7 +1,32 @@
 using Content.Shared._MACRO.CombatMode;
+using Content.Shared.CombatMode;
+using Content.Shared.Mobs;
 
 namespace Content.Server._MACRO.CombatMode;
 
 public sealed partial class CombatModeVisualsSystem : SharedCombatModeVisualsSystem
 {
+    [Dependency] private SharedAppearanceSystem _appearance = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<CombatModeVisualsComponent, ToggleCombatActionEvent>(
+            OnCombatToggle,
+            after: [typeof(SharedCombatModeSystem)]);
+        SubscribeLocalEvent<CombatModeVisualsComponent, MobStateChangedEvent>(OnMobStateChanged);
+    }
+
+    private void OnCombatToggle(Entity<CombatModeVisualsComponent> ent, ref ToggleCombatActionEvent args)
+    {
+        if (TryComp<CombatModeComponent>(ent, out var combat))
+            _appearance.SetData(ent, CombatModeVisualsVisuals.Combat, combat.IsInCombatMode);
+    }
+
+    private void OnMobStateChanged(Entity<CombatModeVisualsComponent> ent, ref MobStateChangedEvent args)
+    {
+        if (args.NewMobState == MobState.Critical || args.NewMobState == MobState.Dead)
+            _appearance.SetData(ent, CombatModeVisualsVisuals.Combat, false);
+    }
 }
